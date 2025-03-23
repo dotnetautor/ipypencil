ba53ded3b342cda7b62678bf90f52b064f3dc963
Matthias Fischer <matthias@dotnetautor.de>
2025-03-23 18:34:01 +0100

    Update project configuration and tests

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,13 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/lib/', '/node_modules/'],
+  modulePathIgnorePatterns: [
+    "<rootDir>/ipypencil/labextension/package.json"
+  ],
   testRegex: '/__tests__/.*.spec.ts[x]?$',
   transformIgnorePatterns: ['/node_modules/(?!(@jupyter(lab|-widgets)/.*)/)'],
   transform: {
     "^.+\\.(ts|tsx)$": ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json', babelConfig: true }]
-  }
+  },
+  setupFiles: ['./jest.setup.js'], // Optional: Add setup file for global mocks
 };
